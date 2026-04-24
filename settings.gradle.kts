809d73bd36a7b3bdbe50b64fe6cd9cb5e122e893
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2026-04-24 13:02:59 +0900

    build: サブプロジェクトのincludeをa-z順にソート

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,9 +14,9 @@ plugins {
 
 rootProject.name = "custom-mdk"
 
-include("26.1-fabric")
-include("26.1-neo")
 include("1.18.2-forge")
 include("1.19.2-forge")
 include("1.20.1-forge")
 include("1.21.1-neo")
+include("26.1-fabric")
+include("26.1-neo")

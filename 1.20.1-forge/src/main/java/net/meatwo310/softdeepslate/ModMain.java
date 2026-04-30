9f1d258c78beb1f5114cf0b9623ef22cea541a3b
Meatwo310 <git@meatwo310.net>
2026-04-30 15:48:57 +0900

    feat(1.20.1-forge): 仮実装

--- 1.20.1-forge/src/main/java/net/meatwo310/softdeepslate/ModMain.java
+++ 1.20.1-forge/src/main/java/net/meatwo310/softdeepslate/ModMain.java
@@ -1,12 +1,118 @@
 package net.meatwo310.softdeepslate;
 
+import net.meatwo310.softdeepslate.config.ServerConfig;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.util.Lazy;
+import net.minecraftforge.event.TagsUpdatedEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.tags.ITag;
+import net.minecraftforge.registries.tags.ITagManager;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 @Mod(Constants.MODID)
 public class ModMain {
+    private static Lazy<Set<Block>> blocksCache = Lazy.of(ModMain::buildCache);
+
     public ModMain(FMLJavaModLoadingContext ctx) {
         Constants.LOGGER.debug(Constants.INITIALIZING, ModUtils.loc("1.20.1-forge"));
-//        ctx.registerConfig(ModConfig.Type.SERVER, ServerConfig.SPEC);
+        ctx.registerConfig(ModConfig.Type.SERVER, ServerConfig.SPEC);
+    }
+
+    private static Set<Block> buildCache() {
+        HashSet<Block> blocks = new HashSet<>();
+
+        for (String name : ServerConfig.BLOCKS.get()) {
+            if (name.startsWith("#")) {
+                cacheBlockTag(blocks, name.substring(1));
+            } else {
+                cacheBlock(blocks, name);
+            }
+        }
+
+        if (blocks.isEmpty()) {
+            Constants.LOGGER.warn("No valid blocks found");
+        } else {
+            Constants.LOGGER.info("Cached {} blocks", blocks.size());
+        }
+
+        return Set.copyOf(blocks);
+    }
+
+    private static void cacheBlock(Set<Block> blocks, String blockName) {
+        ResourceLocation id = ResourceLocation.tryParse(blockName);
+        if (id == null) {
+            return;
+        }
+
+        Optional.ofNullable(ForgeRegistries.BLOCKS.getValue(id)).ifPresentOrElse(
+                block -> {
+                    Constants.LOGGER.debug("Caching block: {}{}", blockName, blocks.contains(block) ? " (DUPLICATED)" : "");
+                    blocks.add(block);
+                },
+                () -> Constants.LOGGER.warn("Unknown block in config: {}", blockName)
+        );
+    }
+
+    private static void cacheBlockTag(Set<Block> blocks, String blockTagName) {
+        ResourceLocation id = ResourceLocation.tryParse(blockTagName);
+        if (id == null) {
+            return;
+        }
+
+        ITagManager<Block> tagManager = ForgeRegistries.BLOCKS.tags();
+        if (tagManager == null) {
+            Constants.LOGGER.warn("Block tag manager is not available: #{}", blockTagName);
+            return;
+        }
+
+        TagKey<Block> tagKey = tagManager.createTagKey(id);
+        if (!tagManager.isKnownTagName(tagKey)) {
+            Constants.LOGGER.warn("Unknown block tag in config: #{}", blockTagName);
+            return;
+        }
+
+        ITag<Block> tag = tagManager.getTag(tagKey);
+        for (Block block : tag) {
+            ResourceLocation blockId = ForgeRegistries.BLOCKS.getKey(block);
+            String blockName = blockId != null ? blockId.toString() : "?";
+
+            Constants.LOGGER.debug(
+                    "Caching block from tag #{}: {}{}",
+                    blockTagName,
+                    blockName,
+                    blocks.contains(block) ? " (DUPLICATED)" : ""
+            );
+            blocks.add(block);
+        }
+    }
+
+    public static boolean shouldMineFaster(BlockState state) {
+        return blocksCache.get().contains(state.getBlock());
+    }
+
+    @Mod.EventBusSubscriber(modid = Constants.MODID)
+    public static class Subscriber {
+        @SubscribeEvent
+        public static void onPlayerBreakSpeed(PlayerEvent.BreakSpeed event) {
+            if (shouldMineFaster(event.getState())) {
+                event.setNewSpeed((float) (event.getNewSpeed() * ServerConfig.MINING_SPEED.get()));
+            }
+        }
+
+        @SubscribeEvent
+        public static void onTagsUpdated(TagsUpdatedEvent event) {
+            blocksCache = Lazy.of(ModMain::buildCache);
+        }
     }
 }
--- 1.20.1-forge/src/main/java/net/meatwo310/softdeepslate/config/ServerConfig.java
+++ 1.20.1-forge/src/main/java/net/meatwo310/softdeepslate/config/ServerConfig.java
@@ -1,9 +1,38 @@
 package net.meatwo310.softdeepslate.config;
 
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.ForgeConfigSpec;
+
+import java.util.List;
 
 public class ServerConfig {
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+
+    public static ForgeConfigSpec.DoubleValue MINING_SPEED = BUILDER
+            .comment("""
+                    Adjusts the mining speed for deepslate.
+                    Given that deepslate is twice as hard as stone, a default value of 2.0 allows it to be mined at the same rate as stone.""")
+            .defineInRange("miningSpeed", 2.0D, 0.0D, Float.MAX_VALUE);
+
+    public static ForgeConfigSpec.ConfigValue<List<? extends String>> BLOCKS = BUILDER
+            .comment("""
+                    List of block IDs or block tags (prefix with #) where this mod adjusts the mining speed.
+                    `/reload` to apply.""")
+            .defineList("blocks", List.of(
+                    "minecraft:deepslate",
+                    "#forge:cobblestone/deepslate",
+                    "#forge:ores_in_ground/deepslate",
+                    "#softdeepslate:building_blocks"
+            ), ServerConfig::isValidIdOrTag);
+
+    private static boolean isValidIdOrTag(Object entry) {
+        return entry instanceof String s && isValidIdOrTag(s);
+    }
+
+    private static boolean isValidIdOrTag(String entry) {
+        String normalized = entry.startsWith("#") ? entry.substring(1) : entry;
+        return ResourceLocation.tryParse(normalized) != null;
+    }
 
     public static final ForgeConfigSpec SPEC = BUILDER.build();
 }
